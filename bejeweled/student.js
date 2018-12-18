bc3b67526b64722b94afa787deb1da1a0d49c659
hansPauwels <43340612+hansPauwels@users.noreply.github.com>
2018-12-18 13:42:58 +0100

    fixed merge conflicts

--- bejeweled/student.js
+++ bejeweled/student.js
@@ -1,1 +1,38 @@
 // Schrijf hier je code
+function width(grid){
+  return grid[0].length;
+}
+
+function height(grid){
+  return grid.length;
+}
+
+function isInside(grid, position){
+  return position.x >= 0 && position.x <= grid.length;
+}
+
+function swap(grid, p, q){
+  let temp = grid[p.y][p.x];
+  grid[p.y][p.x] = grid[q.y][q.x];
+  grid[q.y][q.x] = temp;
+}
+
+function horizontalChainAt(grid, position){
+  let count = 0;
+  for(let i = 0; i !== grid[0].length; i++){
+    if(grid[position.y][position.x] === grid[position.y][i]){
+      count++;
+    }
+  }
+  return count;
+}
+
+function verticalChainAt(grid, position){
+  let count = 0;
+  for(let i = 0; i !== grid.length; i++){
+    if(grid[position.y][position.x] === grid[i][position.x]){
+      count++;
+    }
+  }
+  return count;
+}
